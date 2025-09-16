3054bcc9416661b1de76898e8b95215a50c74bf8
VadimLitau <vadimlitau@yandex.ru>
2025-09-16 19:36:24 +0500

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<script setup>
+import UiButton from '@/shared/Ui/UiButton/UiButton.vue';
+import { ref } from 'vue';
+
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <UiButton>
+    Перейти в каталог
+  </UiButton>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
---
+++ src/shared/Ui/UiButton/UiButton.vue
@@ -0,0 +1,35 @@
+<script script>
+</script>
+
+<template>
+	<button class="btn br-10 w-100">
+    <div class="btn-text">
+      <slot></slot>
+    </div>
+  </button>
+</template>
+
+<style scoped>
+.btn{
+  background: var(--bg-yellow);
+  transition: var(--duration);
+  border: 1px solid transparent;
+
+  &:hover{
+    background: var(--bg-yellow-light);
+  }
+
+  &:active{
+    background: transparent;
+    border: 1px solid var(--text-blue);
+  }
+
+  &:disabled{
+    background: var(--bg-btn-grey);
+  }
+}
+
+.btn-text{
+  color: var(--text-blue);
+}
+</style>
